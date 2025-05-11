fdaeb4eb81662c312c2d1b6a4d8569077891009a
İnci Adıyaman <inci.adiyaman@hotmail.com>
2025-05-11 14:21:13 +0200

    error message is changed

--- PlayerManagement.Model/Requests/CreatePlayerRequest.cs
+++ PlayerManagement.Model/Requests/CreatePlayerRequest.cs
@@ -12,7 +12,7 @@ namespace PlayerManagement.Model.Requests
         public required DateTime DateOfBirth { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
-        [Range(30, 250, ErrorMessage = "Please enter a value bigger than 30")]
+        [Range(30, 250, ErrorMessage = "Please enter a value between 30 and 250")]
         public required int Height { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
--- PlayerManagement.Model/Requests/UpdatePlayerRequest.cs
+++ PlayerManagement.Model/Requests/UpdatePlayerRequest.cs
@@ -12,7 +12,7 @@ namespace PlayerManagement.Model.Requests
         public DateTime DateOfBirth { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
-        [Range(30, 250, ErrorMessage = "Please enter a value bigger than 30")]
+        [Range(30, 250, ErrorMessage = "Please enter a value between 30 and 250")]
         public int Height { get; set; }
 
         [Required(ErrorMessage = "{0} is required")]
